0ea12b5262bb25fe4adbcde949b0dbae8a204a0e
Ketan <ketantoo@gmail.com>
2021-06-25 14:22:01 +0530

    bmi app inputu_page full ui done

--- bmi-calculator-flutter-master/lib/input_page.dart
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const double btmContainerHeight = 80.0;
+const Color bottomPinkColor = Color(0xffeb1555);
+const Color activeCardColor = Color(0xff1d1e33);
+const Color inactiveCardColor = Color(0xff1d1e20);
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +12,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  int height = 80, age = 15, weight = 100;
+  bool male = true, female = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,18 +28,213 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: [
-                ResusableCard(color: Color(0xff1d1e33)),
-                ResusableCard(color: Color(0xff1d1e33)),
+                ResusableCard(
+                  color: male ? inactiveCardColor : activeCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            female = false;
+                            male = true;
+                          });
+                        },
+                        child: Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text("Male"),
+                      )
+                    ],
+                  ),
+                ),
+                ResusableCard(
+                  color: female ? inactiveCardColor : activeCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            female = true;
+                            male = false;
+                          });
+                        },
+                        child: Icon(
+                          FontAwesomeIcons.venus,
+                          size: 80.0,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text("Female"),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          ResusableCard(color: Color(0xff1d1e33)),
+          ResusableCard(
+            color: activeCardColor,
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  weight.toString(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                  ),
+                ),
+                Slider(
+                  activeColor: Colors.pink,
+                  inactiveColor: Colors.pinkAccent,
+                  value: weight.toDouble(),
+                  min: 0.0,
+                  max: 200.0,
+                  onChanged: (value) {
+                    setState(() {
+                      weight = value.toInt();
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: Row(
               children: [
-                ResusableCard(color: Color(0xff1d1e33)),
-                ResusableCard(color: Color(0xff1d1e33)),
+                ResusableCard(
+                  color: activeCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "HEIGHT",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        height.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomIconButton(
+                            icon: Icon(FontAwesomeIcons.minus),
+                            onPress: () {
+                              setState(() {
+                                height--;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          CustomIconButton(
+                            icon: Icon(
+                              FontAwesomeIcons.plus,
+                            ),
+                            onPress: () {
+                              setState(() {
+                                height++;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                ResusableCard(
+                  color: activeCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "AGE",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        age.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomIconButton(
+                            icon: Icon(FontAwesomeIcons.minus),
+                            onPress: () {
+                              setState(() {
+                                age--;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          CustomIconButton(
+                            icon: Icon(
+                              FontAwesomeIcons.plus,
+                            ),
+                            onPress: () {
+                              setState(() {
+                                age++;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              print(age + height);
+            },
+            child: Container(
+              color: bottomPinkColor,
+              height: btmContainerHeight,
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 10),
+              child: Center(
+                child: Text(
+                  "CALCULATE",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                  ),
+                ),
+              ),
             ),
           )
         ],
@@ -39,18 +243,45 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class CustomIconButton extends StatelessWidget {
+  final Icon icon;
+  Function onPress;
+  CustomIconButton({
+    Key key,
+    this.icon,
+    this.onPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: bottomPinkColor,
+        borderRadius: BorderRadius.circular(25.0),
+      ),
+      child: IconButton(
+        onPressed: onPress,
+        icon: icon,
+      ),
+    );
+  }
+}
+
 class ResusableCard extends StatelessWidget {
   final Color color;
+  final Widget cardChild;
   ResusableCard({
-    Key key,
     @required this.color,
-  }) : super(key: key);
+    this.cardChild,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        child: null,
+        child: cardChild,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           color: color,
