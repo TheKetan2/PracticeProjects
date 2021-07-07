9e719ef13aae1ab390e3b755c4870b01c653626a
Ketan <ketantoo@gmail.com>
2021-07-07 12:47:45 +0530

    flutter restart

--- Clima-Flutter-master/lib/main.dart
+++ Clima-Flutter-master/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
